c1e8f9ca99a2cd88590d62f2be615919dcbd451a
jshuam <jmm450@uowmail.edu.au>
2019-03-22 18:22:19 +1100

    (WIP) Skybox Renderer

--- EnginePlayground/SkyboxRenderer.cpp
+++ EnginePlayground/SkyboxRenderer.cpp
@@ -1,10 +1,22 @@
 #include "SkyboxRenderer.h"
 
+SkyboxRenderer::SkyboxRenderer( Loader& loader, const glm::mat4& projection_matrix )
+	:
+	cube( loader.loadToVAO( VERTICES, 3 ) ),
+	texture( loader.loadCubeMap( TEXTURE_FILES ) )
+{
+	shader.start();
+	shader.loadProjectionMatrix( projection_matrix );
+	shader.stop();
+}
 
-
-SkyboxRenderer::SkyboxRenderer()
-{}
-
-
-SkyboxRenderer::~SkyboxRenderer()
-{}
+void SkyboxRenderer::render( const Camera& camera )
+{
+	shader.start();
+	shader.loadViewMatrix( camera );
+	glBindVertexArray( cube.getVaoId() );
+	glEnableVertexAttribArray( 0 );
+	glActiveTexture( GL_TEXTURE0 );
+	glBindTexture( GL_TEXTURE_CUBE_MAP, texture );
+	glDrawArrays( GL_TRIANGLES, 0, cube.getVertexCount() );
+}
--- EnginePlayground/SkyboxRenderer.h
+++ EnginePlayground/SkyboxRenderer.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include "Camera.h"
+#include "Loader.h"
+#include "RawModel.h"
+#include "SkyboxShader.h"
+
 #include <glad/glad.h>
+#include <glm/mat4x4.hpp>
 #include <vector>
 
 class SkyboxRenderer
 {
+public:
+	SkyboxRenderer( Loader& loader, const glm::mat4& projection_matrix );
+
+	void render( const Camera& camera );
+
 private:
 	static constexpr const GLfloat SIZE = 500.0f;
 
@@ -51,5 +62,19 @@ private:
 		-SIZE, -SIZE,  SIZE,
 		 SIZE, -SIZE,  SIZE
 	};
+
+	const std::vector<std::string> TEXTURE_FILES =
+	{
+		"right",
+		"left",
+		"top",
+		"bottom",
+		"back",
+		"front"
+	};
+
+	RawModel cube;
+	GLuint texture;
+	SkyboxShader shader;
 };
 
